75aff3077f11a850d359d9bb0bf38eb2bff53b9a
Ayush <ayushanshu69@gmail.com>
2023-09-23 18:57:34 +0530

    first_commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer () {
+    return (
+        <div className="footer-section">
+            <a href="https://twitter.com/24klabxyz">@24klab</a>
+        </div>
+        
+    )
+}
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export default function Header () {
+    return (
+        <div className="navbar">
+            <a href="/">24klab</a>
+            <div className="nav-content">
+                <a href="/">Home</a>
+                <a href="/projects">Projects</a>
+                <a href="/about">About</a>
+            </div>
+        </div>   
+    )
+}
