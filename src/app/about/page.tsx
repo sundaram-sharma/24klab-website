b43945348202a4c2cf3c52310d0899e3528af6f9
Ayush <ayushanshu69@gmail.com>
2023-10-02 16:06:19 +0530

    added about section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,12 @@
+import '../about/aboutPage.css'
+
 export default function AboutPage () {
     return (
         <div className="container">
-            <h1>Hello</h1>
+            <div className="about-section">
+                <h1>About</h1>
+                <p><b>24klab</b> is founded by <b>Ayush Anshu</b>, to make fun open source tech projects. </p>
+            </div>
         </div>
     )
 }
